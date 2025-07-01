7cccda1cae755b7196495ea2fb3e05660e0fee02
Kalash <trvedikalash091@gmail.com>
2025-07-01 09:14:25 +0530

    FIrst Commit

--- app/src/main/java/com/example/trackbus/MainActivity.java
+++ app/src/main/java/com/example/trackbus/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.student_dashboard);
 
     }
 }
